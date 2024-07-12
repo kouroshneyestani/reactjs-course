b1b69158931fe7dcdaa1c8fbf56abc8aedae5b7b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-12 11:42:16 -0700

    refactor: update file names

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Err2 from "./pages/Examples/ErrorBoundaries/ExampleHocUsage";
 
 import Profiller1 from "./pages/Examples/Profiler/Example1";
 
-import ReactUseRef from "./pages/Examples/ReactHooks/ExampleUseRef";
+import ReactUseRef1 from "./pages/Examples/ReactHooks/ExampleUseRef1";
 import ReactUseRef2 from "./pages/Examples/ReactHooks/ExampleUseRef2";
 import ReactUseRef3 from "./pages/Examples/ReactHooks/ExampleUseRef3";
 
