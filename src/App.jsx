e51d9e3f4191b9fa9d2cf87559807f995193989b
Kourosh <kouroshsharifineyestani@gmail.com>
2024-07-15 14:46:08 +0330

    feat: add App component and write tests for that

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+    return (
+        <div>
+            <h1>Unit Testing</h1>
+            <a href="https://reactjs.org">Learn React</a>
+        </div>
+    );
+}
+
+export default App;
